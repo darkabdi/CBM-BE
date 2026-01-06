7abb02812dba6a0376e89c27e5fd25918e141c3d
Abdi <aabdymohamed@gmail.com>
2026-01-06 17:39:08 +0100

    fixade env skit

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/authRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 dotenv.config();
 connectDB();
 
